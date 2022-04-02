b3aa6442b42a04caa5661b24f51b71426c4f5ec2
amaurysantos27 <amaurysantos.1227@gmail.com>
2022-04-02 13:35:46 -0400

    segunda subida

---
+++ pages/components/titulo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Titulo = () => {
+  return (
+    <div className="flex justify-center py-4 rounded-xl">
+      <div className="container py-2 text-center bg-slate-800 rounded-xl">
+        <h1 className="mb-4 text-3xl font-bold text-center text-white">
+          Aplicacion de carreras
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Titulo;
